93bc89d1c7ca37d929bbf7fee1adad06292499a3
Giuseppe <giusepperocco38@gmail.com>
2022-10-11 15:59:06 +0200

    so_fork modified

--- so_fork/main.c
+++ so_fork/main.c
@@ -12,12 +12,16 @@ int main(){
         //CODICE PADRE
         printf("sono il processo padre(%d)\n",getpid());
         printf("(padre) La varibile PID è %d\n", pid);
+        printf("Prima della wait...\n");
+        wait(NULL);
+        printf("Dopo la wait...\n");
+        
     }
     else {
         //CODICE FIGLIO
         printf("sono il processo figlio(%d)\n",getpid());
         printf("(figlio) La varibile PID è %d\n", pid);
-
+        sleep(10);
     }
 
     return EXIT_SUCCESS;
